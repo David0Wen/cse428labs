dca55f1371a920102b6c1cb635b9625413440cb3
Amygdald <781241140@qq.com>
2023-10-22 12:21:03 -0500

    Updated Lab1

---
+++ lab1/Card_T.h
@@ -0,0 +1,42 @@
+/**
+// File: Card_T.h
+// Author: Ruoyao Wen ruoyao@wustl.edu, Wanzhou Liu l.wanzhou@wustl.edu, Zherui Zhou zherui@wustl.edu
+// Purpose: Declaration of template structure: card, will be used as deck's member variable
+*/
+
+#ifndef _CARD_T_H
+#define _CARD_T_H
+#include <iostream>
+// Include enum class suit as a member of card
+#include "Suit.h"
+
+// Card strucutre: rank and suit 
+template <typename T, typename S>
+class Card
+{
+public:
+	T myRank;
+	Suit mySuit;
+	// Vaule Constructor 
+	Card(T, S);
+};
+
+template <typename Suit, typename Rank>
+bool CompareRank(const Card<Suit, Rank>& card1, const Card<Suit, Rank>& card2);
+
+template <typename Suit, typename Rank>
+bool CompareSuit(const Card<Suit, Rank>& card1, const Card<Suit, Rank>& card2);
+
+// shift operator declaration
+// Output the card feature to std::ostream
+// @param std::ostream & the stream you want to output to
+// @param const Card<T> the object you want to output
+// @return std::ostream &
+template <typename T, typename S>
+std::ostream& operator<<(std::ostream&, const Card<T, S>&);
+
+#ifdef TEMPLATE_HEADERS_INCLUDE_SOURCE /* test whether guard symbol is defined */
+#include "Card_T.cpp"
+#endif
+
+#endif
